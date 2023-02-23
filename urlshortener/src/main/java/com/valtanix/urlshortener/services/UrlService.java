8bbd3b9fca0c523e7936a7ec1f9f18e547bbb839
mrkarthi2000 <103568847+mrkarthi2000@users.noreply.github.com>
2023-02-23 11:50:08 +0530

    unused code removed in service

--- urlshortener/src/main/java/com/valtanix/urlshortener/services/UrlService.java
+++ urlshortener/src/main/java/com/valtanix/urlshortener/services/UrlService.java
@@ -1,9 +1,8 @@
 package com.valtanix.urlshortener.services;
 
-import java.net.URISyntaxException;
 
 public interface UrlService {
 
-    String shortenUrl(String url) throws URISyntaxException;
-    String getOriginalUrl(String url) throws URISyntaxException;
+    String shortenUrl(String url);
+    String getOriginalUrl(String url);
 }
