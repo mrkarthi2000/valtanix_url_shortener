0566bf5d3dbd035666b398255f65536c85b97c19
mrkarthi2000 <103568847+mrkarthi2000@users.noreply.github.com>
2023-02-23 11:49:14 +0530

    unused code removed from resource

--- urlshortener/src/main/java/com/valtanix/urlshortener/resource/UrlShortenerResource.java
+++ urlshortener/src/main/java/com/valtanix/urlshortener/resource/UrlShortenerResource.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.valtanix.urlshortener.services.UrlService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/rest")
@@ -15,14 +14,14 @@ public class UrlShortenerResource {
     private UrlService urlService;
 
     @PostMapping("/example")
-    public ResponseEntity longUrl(@RequestBody UrlServiceRequest urlServicesRequest) throws URISyntaxException {
+    public ResponseEntity longUrl(@RequestBody UrlServiceRequest urlServicesRequest) {
         String longURl=urlServicesRequest.getLongUrl();
         String value=urlService.shortenUrl(longURl);
         return ResponseEntity.ok(value);
     }
 
     @PostMapping("/example1")
-        public ResponseEntity getOriginal(@RequestBody UrlServiceRequest urlServicesRequest ) throws URISyntaxException {
+        public ResponseEntity getOriginal(@RequestBody UrlServiceRequest urlServicesRequest) {
         String original=urlService.getOriginalUrl(urlServicesRequest.getShortenedUrl());
         return ResponseEntity.ok(original);
     }
